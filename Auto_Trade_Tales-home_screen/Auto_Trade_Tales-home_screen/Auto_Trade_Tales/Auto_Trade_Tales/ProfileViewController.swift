5cf58a4cf62dd5d27aa3afcf8613c0f5b9e1d037
p4prabhu <46450896+p4prabhu@users.noreply.github.com>
2023-12-06 21:50:29 -0500

    Add files via upload

---
+++ Auto_Trade_Tales-home_screen/Auto_Trade_Tales-home_screen/Auto_Trade_Tales/Auto_Trade_Tales/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Auto_Trade_Tales
+//
+//  Created by user248810 on 12/6/23.
+//
+import UIKit
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
